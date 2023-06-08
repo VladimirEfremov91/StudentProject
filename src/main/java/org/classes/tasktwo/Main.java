e6ac850938f1841c3031b84769cbeccefe7a6d3b
VladimirEfremov91 <scuderiaferrari63@gmail.com>
2023-06-08 22:37:14 +0400

    Packages done

--- src/main/java/org/classes/tasktwo/Main.java
+++ src/main/java/org/classes/tasktwo/Main.java
@@ -1,6 +1,6 @@
 package org.classes.tasktwo;
 
-import org.classes.taskone.Line;
+import org.classes.tasktwo.Triangle;
 
 import java.util.Scanner;
 
@@ -13,9 +13,17 @@ public class Main {
         double y2 = scanner.nextDouble();
         double x3 = scanner.nextDouble();
         double y3 = scanner.nextDouble();
-
         Point a = new Point(x1, y1);
         Point b = new Point(x2, y2);
         Point c = new Point(x3, y3);
+        Triangle abc = new Triangle(a, b, c);
+        double lenAB = Math.sqrt(Math.pow((b.getX() - a.getX()), 2) - Math.pow((b.getY() - a.getY()), 2));
+        double lenBC = Math.sqrt(Math.pow((c.getX() - b.getX()), 2) - Math.pow((c.getY() - b.getY()), 2));
+        double lenCA = Math.sqrt(Math.pow((a.getX() - c.getX()), 2) - Math.pow((a.getY() - c.getY()), 2));
+        if (lenCA >= lenAB + lenBC || lenAB >= lenCA + lenBC || lenBC >= lenAB + lenCA) {
+            System.out.println("Треугольник не существует");
+        } else {
+            System.out.println(abc.area());
+        }
     }
 }
--- src/main/java/org/classes/tasktwo/Triangle.java
+++ src/main/java/org/classes/tasktwo/Triangle.java
@@ -4,16 +4,13 @@ public class Triangle {
     Point a, b, c;
 
     public Triangle(Point a, Point b, Point c) {
-        double lenAB = Math.sqrt(Math.pow((b.getX() - a.getX()), 2) - Math.pow((b.getY() - a.getY()), 2));
-        double lenBC = Math.sqrt(Math.pow((c.getX() - b.getX()), 2) - Math.pow((c.getY() - b.getY()), 2));
-        double lenCA = Math.sqrt(Math.pow((a.getX() - c.getX()), 2) - Math.pow((a.getY() - c.getY()), 2));
-        if (lenCA == lenAB + lenBC || lenAB == lenCA + lenBC || lenBC == lenAB + lenCA) {
-            System.out.println("Треугольник не существует");
-            return;
-        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
     public double area() {
-        //Напишите свой код здесь
+
+        return 0.5 * Math.abs((b.getX() - a.getX()) * (c.getY() - a.getY()) - (c.getX() - a.getX()) * (b.getY() - a.getY()));
     }
 }
